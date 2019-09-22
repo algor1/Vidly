a77ef1454a01358c1e9acac3d1263310ddbac2eb
igor <alexeev.igor.v@gmail.com>
2019-09-23 00:15:53 +0300

    no message

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -24,21 +24,28 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
-        public ActionResult Random()
-        {
-            var viewModel = new RamdomMovieViewModel();
-            viewModel.Movie = new Movie() { Name = "Shrek!" };
-            viewModel.Customers= new List<Customer>() {
-                new Customer { Id=1, Name="Nik" },
-                new Customer{ Id=2,Name="Bob"}
-            };
-            return View(viewModel);
-    
-        }
+        
         public ActionResult Edit(int id)
         {
-            return Content("id=  "+id);
+            MovieFormViewModel viewModel = new MovieFormViewModel();
+            viewModel.Movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (viewModel == null)
+                return HttpNotFound();
+
+            viewModel.Genres = _context.Genres.ToList();
+            return View("MovieForm", viewModel);
         }
+
+        public ActionResult New()
+        {
+            MovieFormViewModel viewModel = new MovieFormViewModel();
+            
+            viewModel.Genres = _context.Genres.ToList();
+
+            return View("MovieForm", viewModel);
+        }
+
         public ActionResult Index()
         {
             var movies = _context.Movies.Include(c => c.Genre);
@@ -50,12 +57,12 @@ namespace Vidly.Controllers
             var movie=_context.Movies.Include(m => m.Genre).Single(m => m.Id==id);
             return View(movie);
         }
-
-        [Route("movies/released/{year:regex(2015|2016)}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year,int month)
+        [HttpPost]
+        public ActionResult Save(Movie movie)
         {
-            return Content($"year/Month {year}/{month}");
+            
         }
+        
 
     }
 }
