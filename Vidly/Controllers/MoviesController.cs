c6178389d15313c2ced021300ab7d83a1a67c18b
igor <alexeev.igor.v@gmail.com>
2019-09-23 22:25:15 +0300

    Exercise 4

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Data.Entity;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity.Validation;
 
 namespace Vidly.Controllers
 {
@@ -60,7 +61,21 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Movie movie)
         {
-            
+            if (movie.Id == 0)
+            {
+                movie.AddedDate = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDB = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDB.Name = movie.Name;
+                movieInDB.ReleaseDate = movie.ReleaseDate;
+                movieInDB.GenreId = movie.GenreId;
+                movieInDB.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
         
 
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name="Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        
+        [Display(Name = "Added to DataBase")]
         public DateTime AddedDate { get; set; }
+
+        [Required]
+        [Display(Name = "Number in stock")]
         public int NumberInStock { get; set; }
+
+        
         public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
     }
