c3a10cbb9af4e2b8ee9848a2696db7838dde5335
algor1 <alexeev.igor.v@gmail.com>
2019-09-27 16:35:05 +0300

    lection 56

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -50,6 +50,7 @@ namespace Vidly.Controllers
             return View("CustomerForm",viewModel);
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
